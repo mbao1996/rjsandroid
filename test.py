b7a58228906fc0d8715616a86eb82bc6731d83bd
mbao1996 <mbao1996@hotmail.com>
2019-07-18 08:31:34 +0800

    add test

---
+++ test.py
@@ -0,0 +1,33 @@
+# -- coding: utf-8 --
+from appium import webdriver
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '4.4.4'
+desired_caps['deviceName'] = 'b43052abe230'
+desired_caps['appPackage'] = 'com.huawei.android.launcher'
+desired_caps['appActivity'] = 'com.rongjinsuo.android/com.rjs.rongjinsuo.android.splash.SplashActivity'
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+driver.find_element_by_name("1").click()
+driver.find_element_by_name("5").click()
+driver.find_element_by_name("9").click()
+driver.find_element_by_name("9").click()
+driver.find_element_by_name("5").click()
+driver.find_element_by_name("+").click()
+driver.find_element_by_name("6").click()
+driver.find_element_by_name("=").click()
+driver.quit()
+'''
+from appium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+desired_caps = {
+'platformName':'Android',
+'platformVersion':'4.4.4',
+'deviceName':'b43052abe230',
+'appPackage':'com.huawei.android.launcher',
+'appActivity':'com.huawei.android.launcher.splashscreen.StartActivity',
+'appWaitActivity':'com.huawei.android.launcher/.splashscreen.activity.StartActivity',
+'noReset':True,
+}
+
+driver=webdriver.Remote('b43052abe230',desired_caps)
+'''
