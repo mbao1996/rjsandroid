fb076bdf04f72e186bd2534293722e34df065b4a
mbao1996 <mbao1996@hotmail.com>
2019-07-18 17:07:35 +0800

    first try

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
 # -- coding: utf-8 --
 from appium import webdriver
+import time, os
+
+PATH = lambda p:os.path.abspath(os.path.join(os.path.dirname(__file__),p))
+
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '4.4.4'
 desired_caps['deviceName'] = 'b43052abe230'
-desired_caps['appPackage'] = 'com.huawei.android.launcher'
-desired_caps['appActivity'] = 'com.rongjinsuo.android/com.rjs.rongjinsuo.android.splash.SplashActivity'
+desired_caps['app'] = PATH(r"C:\test\com.rongjinsuo.android_v5.5.5_555.apk")
+desired_caps['appPackage'] = 'com.rongjinsuo.android'
+desired_caps['appActivity'] = 'com.rjs.rongjinsuo.android.splash.SplashActivity'
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+time.sleep(5)
+#print(driver.isAppInstalled())
+
+print('---finished---')
+
+'''
 driver.find_element_by_name("1").click()
 driver.find_element_by_name("5").click()
 driver.find_element_by_name("9").click()
@@ -16,7 +27,8 @@ driver.find_element_by_name("+").click()
 driver.find_element_by_name("6").click()
 driver.find_element_by_name("=").click()
 driver.quit()
-'''
+
+
 from appium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 desired_caps = {
